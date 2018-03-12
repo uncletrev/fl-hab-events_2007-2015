384a4f7ff318d74ea989b157c1c39a6aa3cf7ed5
Trevor Conley <32655127+uncletrev@users.noreply.github.com>
2018-03-12 18:00:37 -0400

    fix .csv & console log test

--- js/app.js
+++ js/app.js
@@ -1,15 +1,1 @@
-(function() {
 
-  L.mapbox.accessToken = 'pk.eyJ1Ijoicmdkb25vaHVlIiwiYSI6Im5Ua3F4UzgifQ.PClcVzU5OUj17kuxqsY_Dg';
-
-  var map = L.mapbox.map('map', 'mapbox.light', {
-    zoomSnap: .1,
-    center: [27.92, -83.91],
-    zoom: 7,
-    dragging: true,
-    zoomControl: true,
-    intertia: true
-    //mapbox.light to adjust basemap later on
-  });
-
-})();
