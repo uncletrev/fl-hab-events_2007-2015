a459f6259e569dd647b496f93f76ad399c1875e5
Trevor Conley <32655127+uncletrev@users.noreply.github.com>
2018-03-12 23:43:00 -0400

    road block on ui slider

--- js/app.js
+++ js/app.js
@@ -14,45 +14,102 @@
 
   omnivore.csv('data/hab_events_2007-2015_10000_min.csv')
     .on('ready', function(e) {
-      console.log(e.target.toGeoJSON())
+      drawMap(e.target.toGeoJSON());
     })
     .on('error', function(e) {
       console.log(e.error[0].message);
-    }).addTo(map);
+    });
 
+  function drawMap(data) {
 
-  var legendControl = L.control({
-    position: 'bottomright'
-  });
+    var options = {
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, {
+          radius: 1.5,
+          opacity: 1,
+          weight: 2,
+          fillOpacity: 1,
+        })
+      }
+    }
 
-  legendControl.onAdd = function(map) {
+    var dataLayer = L.geoJson(data, options).addTo(map);
 
-    var legend = L.DomUtil.get("legend");
+    map.fitBounds(dataLayer.getBounds());
 
-    L.DomEvent.disableScrollPropagation(legend);
-    L.DomEvent.disableClickPropagation(legend);
+    map.setZoom(map.getZoom() - .4);
 
-    return legend;
+    console.log(data);
 
-  }
+    resizeCircles(dataLayer, 2007);
 
-  legendControl.addTo(map);
-
-  var sliderControl = L.control({
-    position: 'bottomleft'
-  });
-
-  sliderControl.onAdd = function(map) {
-
-    var controls = L.DomUtil.get("slider");
-
-    L.DomEvent.disableScrollPropagation(controls);
-    L.DomEvent.disableClickPropagation(controls);
-
-    return controls;
+    sequenceUI(dataLayer);
 
   }
 
-  sliderControl.addTo(map);
+  function calcRadius(val) {
+    var radius = Math.sqrt(val / Math.PI);
+    return radius * .003
+  }
+
+  function resizeCircles(dataLayer) {
+
+    dataLayer.eachLayer(function(layer) {
+      var radius = calcRadius(Number(layer.feature.properties['Count_']));
+      layer.setRadius(radius);
+    });
+  }
+
+  function sequenceUI(dataLayer) {
+
+    var sliderControl = L.control({
+      position: 'bottomleft'
+    });
+
+    sliderControl.onAdd = function(map) {
+
+      var slider = L.DomUtil.get("slider");
+
+      L.DomEvent.disableScrollPropagation(slider);
+      L.DomEvent.disableClickPropagation(slider);
+
+      return slider;
+
+    }
+
+    $('#slider input[type=range]')
+      .on('input', function() {
+        var currentYear = this.value;
+
+        resizeCircles(dataLayer, currentYear);
+
+      });
+
+
+    sliderControl.addTo(map);
+
+  }
+
+  function drawLegend(data) {
+
+    var legendControl = L.control({
+      position: 'bottomright'
+    });
+
+    legendControl.onAdd = function(map) {
+
+      var legend = L.DomUtil.get("legend");
+
+      L.DomEvent.disableScrollPropagation(legend);
+      L.DomEvent.disableClickPropagation(legend);
+
+      return legend;
+
+    }
+
+    legendControl.addTo(map);
+
+  }
+
 
 })();
