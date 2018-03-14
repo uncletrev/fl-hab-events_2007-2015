e0162f0ab8348cd51d538f04d31bdd80e95d9a5f
Trevor Conley <32655127+uncletrev@users.noreply.github.com>
2018-03-13 22:18:19 -0400

    week 8 final commit

--- js/app.js
+++ js/app.js
@@ -29,7 +29,8 @@
           opacity: 1,
           weight: 2,
           fillOpacity: 1,
-        })
+          color: getColor(feature.properties.Count_)
+        });
       }
     }
 
@@ -45,6 +46,8 @@
 
     sequenceUI(dataLayer);
 
+    updateMap(dataLayer);
+
   }
 
   function calcRadius(val) {
@@ -52,13 +55,46 @@
     return radius * .003
   }
 
-  function resizeCircles(dataLayer) {
+  function resizeCircles(dataLayer, val) {
 
     dataLayer.eachLayer(function(layer) {
       var radius = calcRadius(Number(layer.feature.properties['Count_']));
       layer.setRadius(radius);
+
+      if (+layer.feature.properties.YEAR != val) {
+        layer.setStyle({
+          opacity: 0,
+          fillOpacity: 0
+        })
+      } else {
+        layer.setStyle({
+          opacity: .9,
+          fillOpacity: .6
+        })
+      }
     });
   }
+
+/* How to get this function working without turning on hidden data layers hidden by line 62?
+  function mouseoverCircles(dataLayer, val) {
+
+    if (+layer.feature.properties.YEAR == val) {
+      layer.on('mouseover', function() {
+        layer.setStyle({
+          opacity: 1,
+          fillOpacity: 1
+        });
+      });
+      layer.on('mouseout', function() {
+        layer.setStyle({
+          opacity: 1,
+          fillOpacity: 1
+        });
+      });
+    }
+  }
+*/
+
 
   function sequenceUI(dataLayer) {
 
@@ -81,14 +117,24 @@
 
     $('#slider input[type=range]')
       .on('input', function() {
-        var sampleYears = 
+        //var sampleYears =
 
         //["2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"]
 
-        resizeCircles(dataLayer, sampleYears);
+        resizeCircles(dataLayer, this.value);
 
       });
 
+  }
+
+  function getColor(val) {
+    if (val < 99999) {
+      return "yellow"
+    } else if (val < 999999) {
+      return "orange"
+    } else if (val > 999999) {
+      return "red"
+    }
   }
 
   function drawLegend(data) {
@@ -112,5 +158,67 @@
 
   }
 
+  function sequenceYear(dataLayer) {
+
+    var yearSelect = L.control({
+      position: 'bottomleft'
+    });
+
+    sliderControl.onAdd = function(map) {
+
+      var sliderYear = ["2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"]
+
+      L.DomEvent.disableScrollPropagation(slider);
+      L.DomEvent.disableClickPropagation(slider);
+
+      return sliderYear;
+
+    }
+
+    sliderControl.addTo(map);
+
+
+    $('#slider input[type=range]')
+      .on('input', function() {
+        //var sampleYears =
+
+        //["2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"]
+
+        resizeCircles(dataLayer, this.value);
+
+      });
+
+  }
+
+  function updateMap(dataLayer) {
+
+    dataLayer.eachLayer(function (layer) {
+
+      var props = layer.feature.properties;
+
+      var tooltipInfo = "<b>" + "Sample Date: " + props["SAMPLE_DAT"] + "</b></br>" + "<b>" + "Location: " + props["LOCATION"] + "</b></br>" + "<b>" + "Karenia brevis Count: " + layer.feature.properties.Count_.toLocaleString() + " cells/liter" + "</b></br>" + "<b>" + "Depth of Sample: " + props["DEPTH"] + " feet" + "</b></br>"
+
+      //wrong year sample dates are ;appearing in the wrong year...
+
+      //need to write function for FWC bacteria count classification and include it in the popup
+
+      layer.bindTooltip(tooltipInfo, {
+        sticky: true,
+        tooltipAnchor: [300, 300]
+      });
+    });
+  }
+
+
 
 })();
+
+//TO-DO LIST:
+
+//write legend function
+//write year slider function
+//write function for FWC bacteria count classifications and include in popup or modify CSV to include classification as a column
+//update descriptive map info
+//fix erroneous data circles that show sample dates in the wrong year
+//fix code to 100% opacity when mouseover
+//add commas to bacteria count
